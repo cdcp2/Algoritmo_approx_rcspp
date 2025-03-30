e88bcd9a5528dcbf216888eca0ad1a295b2669e2
cdcp2 <c.casadiego@uniandes.edu.co>
2025-03-30 16:17:22 -0500

    agregar dependencias y versión inicial del algoritmo

---
+++ rcspp_approx/target/debug/deps/rcspp_approx-7f98a291e56f9220.d
@@ -0,0 +1,5 @@
+C:\Users\cdcp2\Desktop\Analisis_de_AlgoritmosRUST\Algoritmo_genetico_rcspp\rcspp_approx\target\debug\deps\librcspp_approx-7f98a291e56f9220.rmeta: src\main.rs
+
+C:\Users\cdcp2\Desktop\Analisis_de_AlgoritmosRUST\Algoritmo_genetico_rcspp\rcspp_approx\target\debug\deps\rcspp_approx-7f98a291e56f9220.d: src\main.rs
+
+src\main.rs:
---
+++ rcspp_approx/target/debug/deps/rcspp_approx-d20a87fe0a7040c9.d
@@ -0,0 +1,5 @@
+C:\Users\cdcp2\Desktop\Analisis_de_AlgoritmosRUST\Algoritmo_genetico_rcspp\rcspp_approx\target\debug\deps\librcspp_approx-d20a87fe0a7040c9.rmeta: src\main.rs
+
+C:\Users\cdcp2\Desktop\Analisis_de_AlgoritmosRUST\Algoritmo_genetico_rcspp\rcspp_approx\target\debug\deps\rcspp_approx-d20a87fe0a7040c9.d: src\main.rs
+
+src\main.rs:
